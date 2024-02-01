b9eea4cd55c3e03fa57f6ca361112b730de7b5e3
H-isaac23 <dev.isaac23@gmail.com>
2024-02-01 11:52:47 +0800

    add new tests

--- src/queue/queue.controller.spec.ts
+++ src/queue/queue.controller.spec.ts
@@ -1,20 +1,112 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QueueController } from './queue.controller';
 import { QueueService } from './queue.service';
+import { Queue } from '@prisma/client';
+import { plainToInstance } from 'class-transformer';
+import { CreateQueueDto } from './dto/create-queue.dto';
+import { validate } from 'class-validator';
+
+const mockCustomer: Queue[] = [
+  {
+    queueId: 1,
+    name: 'Audrey Hall',
+    email: 'audrey@gmail.com',
+    contactNumber: '09123456789',
+    queueStatus: 'waiting',
+    createdAt: new Date('2024-01-31T06:43:45.698Z'),
+  },
+  {
+    queueId: 2,
+    name: 'Fors Wall',
+    email: 'fors@gmail.com',
+    contactNumber: '09123456781',
+    queueStatus: 'accommodated',
+    createdAt: new Date('2024-01-31T07:47:35.237Z'),
+  },
+];
 
 describe('QueueController', () => {
   let controller: QueueController;
+  let service: QueueService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [QueueController],
-      providers: [QueueService],
+      providers: [
+        {
+          provide: QueueService,
+          useValue: {
+            createQueueCustomer: jest.fn().mockResolvedValue(mockCustomer[0]),
+            findAllQueueCustomers: jest
+              .fn()
+              .mockResolvedValueOnce(mockCustomer),
+            findFilteredQueueCustomers: jest.fn(),
+            updateQueueCustomerStatus: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<QueueController>(QueueController);
+    service = module.get<QueueService>(QueueService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createQueueCustomer', () => {
+    it('should create a queue customer', async () => {
+      const response = await controller.createQueueCustomer({
+        name: 'Audrey Hall',
+        email: 'audrey@gmail.com',
+        contactNumber: '09123456789',
+      });
+
+      expect(response).toBe(mockCustomer[0]);
+    });
+
+    it('should not create a customer without name', async () => {
+      const queueDto = {
+        name: '',
+      };
+
+      const ofQueueDto = plainToInstance(CreateQueueDto, queueDto);
+      const error = await validate(ofQueueDto);
+
+      expect(error.length).not.toBe(0);
+      expect(JSON.stringify(error)).toContain('name should not be empty');
+    });
+  });
+
+  describe('findFilteredQueueCustomers', () => {
+    it('should return only waiting customers', async () => {
+      jest
+        .spyOn(service, 'findFilteredQueueCustomers')
+        .mockImplementationOnce(async () => {
+          return [mockCustomer[0]];
+        });
+
+      const response = await controller.findFilteredQueueCustomers('waiting');
+      expect(service.findFilteredQueueCustomers).toHaveBeenCalledWith(
+        'waiting',
+      );
+      expect(response).toStrictEqual([mockCustomer[0]]);
+    });
+
+    it('should return only accommodated customers', async () => {
+      jest
+        .spyOn(service, 'findFilteredQueueCustomers')
+        .mockImplementationOnce(async () => {
+          return [mockCustomer[1]];
+        });
+
+      const response =
+        await controller.findFilteredQueueCustomers('accommodated');
+      expect(service.findFilteredQueueCustomers).toHaveBeenCalledWith(
+        'accommodated',
+      );
+      expect(response).toStrictEqual([mockCustomer[1]]);
+    });
   });
 });
--- src/queue/queue.service.spec.ts
+++ src/queue/queue.service.spec.ts
@@ -1,52 +1,97 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QueueService } from './queue.service';
+import { PrismaService } from '../prisma.service';
+import { Queue } from '@prisma/client';
+
+const mockCustomer: Queue[] = [
+  {
+    queueId: 1,
+    name: 'Audrey Hall',
+    email: 'audrey@gmail.com',
+    contactNumber: '09123456789',
+    queueStatus: 'waiting',
+    createdAt: new Date('2024-01-31T06:43:45.698Z'),
+  },
+  {
+    queueId: 2,
+    name: 'Fors Wall',
+    email: 'fors@gmail.com',
+    contactNumber: '09123456781',
+    queueStatus: 'accommodated',
+    createdAt: new Date('2024-01-31T07:47:35.237Z'),
+  },
+];
+
+const db = {
+  queue: {
+    findMany: jest.fn(),
+    create: jest.fn().mockResolvedValueOnce(mockCustomer[0]),
+  },
+};
 
 describe('QueueService', () => {
   let service: QueueService;
-  const mockCustomer = [
-    {
-      name: 'Audrey Hall',
-      email: 'audreyhall@gmail.com',
-      number: '09123456789',
-      queueStatus: 'Finished',
-    },
-    {
-      name: 'Fors Wall',
-      email: 'forswall@gmail.com',
-      number: '09123456788',
-      queueStatus: 'Waiting',
-    },
-  ];
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QueueService],
+      providers: [
+        QueueService,
+        {
+          provide: PrismaService,
+          useValue: db,
+        },
+      ],
     }).compile();
 
     service = module.get<QueueService>(QueueService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
+  describe('createQueueCustomer', () => {
+    it('should create a queue customer', async () => {
+      const data = {
+        name: 'Audrey Hall',
+        email: 'audrey@gmail.com',
+        contactNumber: '09123456789',
+      };
+
+      const response = await service.createQueueCustomer(data);
+      expect(response).toBe(mockCustomer[0]);
+    });
+  });
+
   describe('findQueueCustomers', () => {
     it('should return an array of all queue customers', async () => {
-      const response = await service.findQueueCustomers();
+      jest.spyOn(prisma.queue, 'findMany').mockResolvedValueOnce(mockCustomer);
+
+      const response = await service.findAllQueueCustomers();
       expect(response).toBeInstanceOf(Array);
       expect(response).toBe(mockCustomer);
     });
 
     it('should return an array of only waiting customers', async () => {
-      const response = await service.findQueueCustomers('Waiting');
+      jest
+        .spyOn(prisma.queue, 'findMany')
+        .mockResolvedValueOnce([mockCustomer[0]]);
+
+      const response = await service.findFilteredQueueCustomers('waiting');
       expect(response).toBeInstanceOf(Array);
-      expect(response).toBe([mockCustomer[1]]);
+      expect(response).toStrictEqual([mockCustomer[0]]);
     });
 
     it('should return an array of only finished customers', async () => {
-      const response = await service.findQueueCustomers('Finished');
+      jest
+        .spyOn(prisma.queue, 'findMany')
+        .mockResolvedValueOnce([mockCustomer[1]]);
+
+      const response = await service.findFilteredQueueCustomers('accommodated');
       expect(response).toBeInstanceOf(Array);
-      expect(response).toBe([mockCustomer[0]]);
+      expect(response).toStrictEqual([mockCustomer[1]]);
     });
   });
 });
